7c53cafdef695e4c0abef1f003926631d48eb585
kmafeni04 <komemafeni944@gmail.com>
2024-09-08 23:22:43 +0100

    testing function higlighting

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,9 +1,17 @@
 ; highlights.scm
 
 (identifier) @variable
+(assignment
+    (identifier)
+    (operator)
+    (function_declaration )) @function
 (number) @number
 [ (function_declaration) (function_call) ] @function
-(parameter_list) @variable.parameter
+(function_declaration
+(parameter_list 
+  (identifier) @variable.parameter)
+)
+ (function_call (identifier) @variable.parameter) @function
 (string) @string
 (comment) @comment
 (operator) @operator
